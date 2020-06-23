baa9894ea89aae407e054e5652b95d46a65acaa5
Cris <i1610466@continental.edu.pe>
2020-06-23 02:39:18 -0500

    agregando circularimage

--- app/src/main/java/com/example/splash/ClientesDetalleActivity.java
+++ app/src/main/java/com/example/splash/ClientesDetalleActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
 import com.example.splash.helpers.QueueUtils;
 import com.example.splash.models.Clientes;
 
@@ -14,6 +16,8 @@ public class ClientesDetalleActivity extends AppCompatActivity {
     QueueUtils.QueueObject queue = null;
     int clientesId;
     Clientes clientesObject = new Clientes(0, "", "", "");
+    ImageLoader imageLoader;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,10 +30,13 @@ public class ClientesDetalleActivity extends AppCompatActivity {
         /// consumimos informacion detallada de la nube
         clientesObject.id = clientesId;
         queue = QueueUtils.getInstance(this.getApplicationContext());
+        imageLoader = queue.getImageLoader();
         Clientes.injectContactFromCloud(queue, clientesObject, this);
     }
     public void refresh(){
         TextView txtNombre = findViewById(R.id.txtNombre);
         txtNombre.setText(clientesObject.nickname);
+        NetworkImageView imgFoto = findViewById(R.id.imgFoto);
+        imgFoto.setImageUrl(clientesObject.urlImage, imageLoader);
     }
 }
